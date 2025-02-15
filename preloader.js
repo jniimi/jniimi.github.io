ab85c138e42b2cf54f3e3f82a5be967e6dece03e
Junichiro Niimi <54612149+jniimi@users.noreply.github.com>
2025-02-16 06:41:18 +0900

    add timestamp

--- preloader.js
+++ preloader.js
@@ -1,11 +1,14 @@
 window.addEventListener("load", () => {
     const preloader = document.getElementById("preloader");
     const content = document.getElementById("content");
+    const webpUrl = `preloader.webp?nocache=${new Date().getTime()}`;
 
-    const logoDuration = 3000;
+    preloader.innerHTML = `<img id="webpAnim" src="${webpUrl}" alt="Lab Logo Animation">`;
+
+    const webpDuration = 3000;
 
     setTimeout(() => {
         preloader.style.display = "none";
         content.style.display = "flex";
-    }, logoDuration);
+    }, webpDuration);
 });
